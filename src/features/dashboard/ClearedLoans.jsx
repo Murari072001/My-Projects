b96082be7944fe6b6f5156cd257b9cdb42075bb7
Murari072001 <murari.t2001@gmail.com>
2024-04-03 12:52:13 +0530

    asda

---
+++ src/features/dashboard/ClearedLoans.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { useClearedLoansQuery } from "../../services/jsonApi";
+import { Link, useNavigate } from "react-router-dom";
+
+function ClearedLoans() {
+    const { data, isLoading } = useClearedLoansQuery()
+    const [time, setDate] = useState('')
+    const navigate = useNavigate()
+    useEffect(() => {
+        let temp = new Date()
+        setDate(new Date(temp.getFullYear(), temp.getMonth(), 1, 0, 0, 0))
+    }, [])
+    function checkDate(date) {
+        let time = new Date(date)
+        return `${time.getDate()}/${time.getMonth() + 1}/${time.getFullYear()}`
+    }
+    return <div className="container m-2">
+        <table className="table table-striped table-success table-hover">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Account Number</th>
+                    <th>Mobile Number</th>
+                    <th>EMI Amount</th>
+                    <th>Last EMI Paid On</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data?.filter((ele) => ele.emiPaid[ele.emiPaid.length - 1]?.paidOn >= time).map((a) => {
+                    return <tr>
+                        <td>{a.firstname} {a.lastname}</td>
+                        <td><Link to={`/admindashboard/allLoans/${a.accountNumber}`}>{a.accountNumber}</Link></td>
+                        <td>{a.phoneNumber}</td>
+                        <td>₹{a.emiPaid[a.emiPaid.length - 1].emiAmount.toLocaleString("en-IN")}</td>
+                        <td>{checkDate(a.emiPaid[a.emiPaid.length - 1].paidOn)}</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    </div>
+}
+
+export default ClearedLoans  
